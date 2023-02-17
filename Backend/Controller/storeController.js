9608ada0efb64a821e624809f14c697487ede420
Rohan Tiwari <ping.techyrohan@gmail.com>
2023-02-17 23:24:03 +0545

    added comments

--- Backend/Controller/storeController.js
+++ Backend/Controller/storeController.js
@@ -1,6 +1,7 @@
 const Store = require("../Model/storeModel");
 const catchAsync = require("../Utils/CatchAsync");
 
+// create new store
 exports.createStore = catchAsync(async (request, response) => {
   request.body.storeBy = request.user._id;
   console.log(request.body);
@@ -12,6 +13,7 @@ exports.createStore = catchAsync(async (request, response) => {
   });
 });
 
+// get store near user location
 exports.nearStore = catchAsync(async (request, response, next) => {
   const longitude = parseFloat(request.body.longitude);
   const latitude = parseFloat(request.body.latitude);
