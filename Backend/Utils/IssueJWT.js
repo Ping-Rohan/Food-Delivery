b0029843f8b57d89c9f37ed8fa7d57d8974099fd
Rohan Tiwari <ping.techyrohan@gmail.com>
2023-02-18 20:11:38 +0545

    Implemented refresh access token

--- Backend/Utils/IssueJWT.js
+++ Backend/Utils/IssueJWT.js
@@ -1,4 +1,7 @@
 const jwt = require("jsonwebtoken");
+const User = require("../Model/userModel");
+const AppError = require("./AppError");
+const CatchAsync = require("./CatchAsync");
 
 exports.issueAccessToken = (payload) => {
   return jwt.sign(payload, process.env.ACCESS_JWT, {
@@ -11,3 +14,28 @@ exports.issueRefreshToken = (payload) => {
     expiresIn: "1d",
   });
 };
+
+exports.refreshAccessToken = CatchAsync(async (request, response, next) => {
+  if (!request.cookies.auth)
+    return next(
+      new AppError("Please provide refresh token to refresh access token")
+    );
+
+  const refreshToken = request.cookies.auth;
+  const decoded = jwt.decode(refreshToken);
+
+  const userDocument = await User.findById(decoded._id);
+  if (!userDocument) return next(new AppError("User doesnot exist"));
+
+  // this points to the global
+  const newRefreshToken = this.issueRefreshToken({ _id: userDocument._id });
+  const newAccessToken = this.issueAccessToken({ _id: userDocument._id });
+
+  response.cookie("auth", newRefreshToken, {
+    httpOnly: true,
+  });
+
+  response.status(200).json({
+    accessToken: newAccessToken,
+  });
+});
